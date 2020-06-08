b22e8eecc28443a5992436317461b22465a24330
AoiHosizora <a970335605@hotmail.com>
2020-06-08 11:40:16 +0800

    feat(sh): finish jar2pom.sh for maven

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Generate_jar_pom
 
 + A script to generate maven's pom.xml from a directory of jar files
+
+### Function
+
++ [x] Generate xml file for maven-build jar (read pom.properties file)
++ [x] Filter non-jar(skip), break-jar(skip), non-maven(error), multiple-properties(error) files
++ [ ] Generate for non-maven-build jar
+
+### Usage
+
+```text
+Usage of jar2pom.sh:
+    sh jar2pom.sh [-h|--help]: show help
+    sh jar2pom.sh $DIRECTORY: generate maven xml to out.xml
+    sh jar2pom.sh $DIRECTORY $OUTPUT: generate maven xml to $OUTPUT
+```
+
+### Result
+
+```xml
+<!-- GENERATE BY jar2pom.sh -->
+
+<!-- xxx/commons-io-2.4.jar -->
+<dependency>
+    <groupId>commons-io</groupId>
+    <artifactId>commons-io</artifactId>
+    <version>2.4</version>
+</dependency>
+
+<!-- END OF DIRECTORY xxx -->
+```
--- jar2pom.sh
+++ jar2pom.sh
@@ -1,43 +1,112 @@
-error=()
+SH_NAME="jar2pom.sh"
+OUTPUT=${2:-"out.xml"}
+PWD="$( cd "$( dirname "$0" )" && pwd )"
+
+# Print help
+if [ "$1" == "" -o "$1" == "-h" -o "$1" == "--help" ];
+then
+    echo "Usage of $SH_NAME:"
+    echo "    sh $SH_NAME [-h|--help]: show help"
+    echo "    sh $SH_NAME \$DIRECTORY \$OUTPUT: generate maven xml to \$OUTPUT, $OUTPUT default for out.xml"
+    exit
+fi
+
+# Init array and output file
+echo ""
+echo "> Start generating:"
 success=()
+error=()
+skip=()
+echo "<!-- GENERATE BY $SH_NAME -->" > $OUTPUT
+
+# Start
 for entry in "$1"/*;
 do
-    # echo "$entry"
-    prop=$(jar -tf "$entry" | grep -E "META-INF/maven/.*/pom.properties")
-    if [ "$prop" != "" -a $(echo "$prop" | wc -l) == 1 ];
+    # Check jar file
+    if [[ "$entry" != *.jar ]];
     then
-        content=$(unzip -q -c "$entry" "$prop")
-        ver=$(echo "$content" | grep -P "(?<=version=).+" -o)
-        gid=$(echo "$content" | grep -P "(?<=groupId=).+" -o)
-        aid=$(echo "$content" | grep -P "(?<=artifactId=).+" -o)
-        if [ "$gid" != "" -a "$aid" != "" ];
+        echo "$entry -> skip (non-jar)"
+        skip+=("$entry -> skip (non-jar)")
+        continue
+    fi
+
+    # Read jar file
+    jar=$( jar -tf "$entry" 2> /dev/null )
+    if [ $? -ne 0 ]; 
+    then
+        echo "$entry -> skip (break-jar)"
+        skip+=("$entry -> skip (break-jar)")
+        continue
+    fi
+
+    # Read pom.properties
+    prop=$( echo "$jar" | grep -E "META-INF/maven/.*/pom.properties" )
+    if [ "$prop" == "" ];
+    then
+        echo "$entry -> error (non-maven)"
+        error+=("$entry -> error (non-maven)")
+    elif [ $( echo "$prop" | wc -l ) != 1 ];
+    then
+        echo "$entry -> error (multiple-properties)"
+        error+=("$entry -> error (multiple-properties)")
+    else
+        # Get properties content
+        content=$( unzip -q -c "$entry" "$prop" )
+        ver=$( echo "$content" | grep -P "(?<=version=).+" -o )
+        gid=$( echo "$content" | grep -P "(?<=groupId=).+" -o )
+        aid=$( echo "$content" | grep -P "(?<=artifactId=).+" -o )
+        if [ "$gid" == "" -a "$aid" == "" -a "$ver" == "" ];
         then
+            echo "$entry -> error (empty-properties)"
+            error+=("$entry -> error (empty-properties)")
+        else
+            # Write to output
+            echo "" >> $OUTPUT
+            echo "<!-- $entry -->" >> $OUTPUT
+            echo "<dependency>" >> $OUTPUT
+            echo "    <groupId>$gid</groupId>" >> $OUTPUT
+            echo "    <artifactId>$aid</artifactId>" >> $OUTPUT
+            echo "    <version>$ver</version>" >> $OUTPUT
+            echo "</dependency>" >> $OUTPUT
+
+            echo "$entry -> $gid:$aid:$ver"
             success+=("$entry")
-            echo ""
-            echo "<!-- $entry -->"
-            echo "<dependency>"
-            echo "    <groupId>$gid</groupId>"
-            echo "    <artifactId>$aid</artifactId>"
-            echo "    <version>$ver</version>"
-            echo "</dependency>"
-            continue
         fi
     fi
-    error+=("$entry")
 done
 
+# End of output file
+echo "" >> $OUTPUT
+echo "<!-- END OF DIRECTORY $1 -->" >> $OUTPUT
+
+# Log result
+skip_cnt=${#skip[*]}
 success_cnt=${#success[*]}
 fail_cnt=${#error[*]}
 
+# Success
 echo ""
-echo "Success to generate pom for $success_cnt file(s) above."
+echo "> Success to generate pom for $success_cnt file(s) above."
+echo "> You can find it in $PWD/$OUTPUT."
 
+# Skip
+if [ $skip_cnt -gt 0 ];
+then
+    echo ""
+    echo "> Skip $skip_cnt non-jar or break-jar file(s):"
+    for ((i = 0; i < ${#skip[@]}; i++))
+    do
+        echo "${skip[$i]}"
+    done
+fi
+
+# Fail
 if [ $fail_cnt -gt 0 ];
 then
     echo ""
-    echo "Failed to generate pom for $fail_cnt file(s):"
-    for entry in ${error[*]};
+    echo "> Failed to generate pom for $fail_cnt file(s):"
+    for ((i = 0; i < ${#error[@]}; i++))
     do
-        echo "$entry"
+        echo "${error[$i]}"
     done
 fi
